ce5e6b5947884214f81a623bf219cd325e49bab6
HuberKazoku <HuberKazoku@HuberKazoku-PC>
2017-05-10 11:40:16 -0600

    5/10/2017 Turned class into a Java Bean

--- BigFishChallenge/src/byui/cit260/bigFishChallenge/model/Fish.java
+++ BigFishChallenge/src/byui/cit260/bigFishChallenge/model/Fish.java
@@ -5,10 +5,88 @@
  */
 package byui.cit260.bigFishChallenge.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 /**
  *
- * @author shaunathan
+ * @author Kelly
  */
-public class Fish {
+public class Fish implements Serializable{
+    // Class instance variables
+    private String type;
+    private double length;
+    private double weight;
+
+    public Fish() {
+    }
+    
+    
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.type);
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.length) ^ (Double.doubleToLongBits(this.length) >>> 32));
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.weight) ^ (Double.doubleToLongBits(this.weight) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" + "type=" + type + ", length=" + length + ", weight=" + weight + '}';
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fish other = (Fish) obj;
+        if (Double.doubleToLongBits(this.length) != Double.doubleToLongBits(other.length)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.weight) != Double.doubleToLongBits(other.weight)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
     
 }
