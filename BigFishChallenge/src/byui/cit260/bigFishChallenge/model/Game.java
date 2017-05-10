bc44982a2c57b3b89ef120bef1ace8762e5372f2
HuberKazoku <HuberKazoku@HuberKazoku-PC>
2017-05-10 11:40:40 -0600

    5/10/2017 Turned class into a Java Bean

--- BigFishChallenge/src/byui/cit260/bigFishChallenge/model/Game.java
+++ BigFishChallenge/src/byui/cit260/bigFishChallenge/model/Game.java
@@ -5,10 +5,58 @@
  */
 package byui.cit260.bigFishChallenge.model;
 
+import java.io.Serializable;
+
 /**
  *
- * @author shaunathan
+ * @author Kelly
  */
-public class Game {
+public class Game implements Serializable{
+    // class instance variables
+    private double totalMoves;
+
+    public Game() {
+    }
+    
+    
+
+    public double getTotalMoves() {
+        return totalMoves;
+    }
+
+    public void setTotalMoves(double totalMoves) {
+        this.totalMoves = totalMoves;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 23 * hash + (int) (Double.doubleToLongBits(this.totalMoves) ^ (Double.doubleToLongBits(this.totalMoves) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "totalMoves=" + totalMoves + '}';
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (Double.doubleToLongBits(this.totalMoves) != Double.doubleToLongBits(other.totalMoves)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
