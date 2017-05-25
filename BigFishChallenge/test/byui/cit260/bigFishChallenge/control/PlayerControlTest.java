a5c117b099fe709f1d79a52d21077dd5331d5a77
shaunathan <shaunathan@Family-PC>
2017-05-24 19:16:19 -0700

    Added Test Cases to PlayerControlTest for checkWeight function

--- BigFishChallenge/test/byui/cit260/bigFishChallenge/control/PlayerControlTest.java
+++ BigFishChallenge/test/byui/cit260/bigFishChallenge/control/PlayerControlTest.java
@@ -31,6 +31,50 @@ public class PlayerControlTest {
         assertEquals(expResult, result, 0.01);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+        
+        System.out.println("checkWeight Case 2");
+        inventoryWeight = -1;
+        newWeight = 10;
+        expResult = -1;
+        result = instance.checkWeight(inventoryWeight, newWeight);
+        assertEquals(expResult, result, 0.01);
+        
+        System.out.println("checkWeight Case 3");
+        inventoryWeight = 50;
+        newWeight = -1;
+        expResult = -1;
+        result = instance.checkWeight(inventoryWeight, newWeight);
+        assertEquals(expResult, result, 0.01);
+        
+        System.out.println("checkWeight Case 4");
+        inventoryWeight = 50;
+        newWeight = 51;
+        expResult = -1;
+        result = instance.checkWeight(inventoryWeight, newWeight);
+        assertEquals(expResult, result, 0.01);
+        
+        System.out.println("checkWeight Case 5");
+        inventoryWeight = 100;
+        newWeight = 0;
+        expResult = 0;
+        result = instance.checkWeight(inventoryWeight, newWeight);
+        assertEquals(expResult, result, 0.01);
+        
+        System.out.println("checkWeight Case 6");
+        inventoryWeight = 0;
+        newWeight = 100;
+        expResult = 0;
+        result = instance.checkWeight(inventoryWeight, newWeight);
+        assertEquals(expResult, result, 0.01);
+        
+        System.out.println("checkWeight Case 7");
+        inventoryWeight = 50;
+        newWeight = 50;
+        expResult = 0;
+        result = instance.checkWeight(inventoryWeight, newWeight);
+        assertEquals(expResult, result, 0.01);
+        
+        
     }
     
 }
