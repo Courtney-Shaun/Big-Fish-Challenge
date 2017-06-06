38e132cd7bcaecbcd90ec366e66ab0cca04bdb46
HuberKazoku <HuberKazoku@HuberKazoku-PC>
2017-06-06 17:18:05 -0600

    Added CarryWeightView

---
+++ BigFishChallenge/src/byui/cit260/bigFishChallenge/view/CarryWeightView.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.bigFishChallenge.view;
+
+import java.util.Scanner;
+import byui.cit260.bigFishChallenge.control.PlayerControl;
+
+/**
+ *
+ * @author HuberKazoku
+ */
+
+public class CarryWeightView {
+    
+}
