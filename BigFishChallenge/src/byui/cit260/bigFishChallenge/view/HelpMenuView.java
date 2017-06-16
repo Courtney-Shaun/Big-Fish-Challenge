9f41585d7edfb72b36d4312e574702813e29bd25
Cody <Cody@DESKTOP-PC>
2017-06-16 09:01:31 -0700

    added text to help menu items

--- BigFishChallenge/src/byui/cit260/bigFishChallenge/view/HelpMenuView.java
+++ BigFishChallenge/src/byui/cit260/bigFishChallenge/view/HelpMenuView.java
@@ -58,22 +58,71 @@ public class HelpMenuView  extends View{
     }
 
     private void viewObjective() {
-        System.out.println("*** viewObjective function called ***");
+        System.out.println("\n"
+              + "\n--------------------------------"
+              + "\n Objective of the Game         |"
+              + "\n--------------------------------"
+              + "\n The objective of Big Fish"
+              + "\n challenge is to catch 40lb of"
+              + "\n fish before the day is over."
+              + "\n--------------------------------");
     }
 
     private void aboutFuel() {
-        System.out.println("*** aboutFuel function called ***");
+        System.out.println("\n"
+              + "\n--------------------------------"
+              + "\n About Fuel                    |"
+              + "\n--------------------------------"
+              + "\n You must always have fuel in"
+              + "\n your tank to move around. As"
+              + "\n you move, keep an eye on your"
+              + "\n fuel level. If you run out of"
+              + "\n fuel while you are on the"
+              + "\n water, you lose the game. You"
+              + "\n can buy more fuel at the"
+              + "\n marina."
+              + "\n--------------------------------");
     }
 
     private void aboutBait() {
-        System.out.println("*** aboutBait function called ***");
+        System.out.println("\n"
+              + "\n--------------------------------"
+              + "\n About Bait                    |"
+              + "\n--------------------------------"
+              + "\n You can't fish without bait."
+              + "\n As you fish, your bait"
+              + "\n inventory will decrease. If you"
+              + "\n run out, you will have to go"
+              + "\n back to the marina to buy more."
+              + "\n--------------------------------");
     }
 
     private void aboutCastLine() {
-        System.out.println("*** aboutCastLine function called ***");
+        System.out.println("\n"
+              + "\n--------------------------------"
+              + "\n About Casting                  |"
+              + "\n--------------------------------"
+              + "\n To catch fish, you must cast"
+              + "\n your line into the water. When"
+              + "\n you cast, you may feel a tug on"
+              + "\n the line. You must then choose"
+              + "\n how hard you must pull to set"
+              + "\n the hook in the fish's mouth."
+              + "\n Fish you catch are addeded to"
+              + "\n your inventory."
+              + "\n--------------------------------");
     }
     
     private void interpretClues() {
-        System.out.println("*** interpretClues function called ***");
+        System.out.println("\n"
+              + "\n--------------------------------"
+              + "\n About Clues                   |"
+              + "\n--------------------------------"
+              + "\n There are experts about fishing"
+              + "\n on Greer's Ferry Lake all over."
+              + "\n Be sure you talk to them to"
+              + "\n get clues on where you can go"
+              + "\n to find the big fish."
+              + "\n--------------------------------");
     }
 }
